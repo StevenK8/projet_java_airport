601b5440a36cac300fde64e9b313eae97aa92331
damie <damien.chancerel@gmail.com>
2020-01-05 20:41:08 +0100

    update documentation generale

--- airport/src/Main/Main.java
+++ airport/src/Main/Main.java
@@ -22,18 +22,7 @@ public class Main {
 	
 	/** 
 	 * @param args
-	 * Documentation generale :
-	 * Cette application permet de gerer un aeroport. Elle met l'utilisateur a la place d'un controleur aerien.
-	 * Apres avoir renseigne le nombre de pistes que l'aeroport possede, plusieurs avions apparaissent dans les radars 
-	 * et l'utilisateur gere leur atterrissages. Il est aussi en charge du remplissage des avions et de leur decollage.
-	 * Le remplissage de chaque avion present "sur le tarmac" depend des personnes presente dans l'aeroport.
-	 * Il peut s'agir de pilotes, de personnels navigants mais aussi de simples passagers.
-	 * Lorsque qu'un certains nombre de contraintes sont respectees (pauses des pilotes, bonne compagnie aerienne, pauses avant 
-	 * un prochain decollage...) un avion est rempli et peut decoller.
-	 * L'atterrissage se gere de la meme façon : l'utilisateur peut gerer les priorites d'atterrissages de maniere automatique.
-	 * Il peut aussi consulter l'etat des avions, des passagers et de toutes les entites en attente dans son aeroport.
-	 * L'application se deroule sous formes d'intervalles d'actions. Il y a en tout 7 actions à chaque "tour" de jeu qui se repeteront.
-	 * A la fin de chaque tour, l'utilisateur peut aussi realiser certaines operations speciales comme la fermeture d'une piste ou sa reouverture.
+	 * 
 	 */
 	public static void main(String[] args) {	
 		//Creation de laeroport
